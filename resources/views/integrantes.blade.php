9cca8fab207ea33ef4673327f49a40c9c475ff08
jvalladolid365 <jvalladolid365@gmail.com>
2022-11-11 03:22:44 -0500

    cambio de informacion integrantes

--- resources/views/integrantes.blade.php
+++ resources/views/integrantes.blade.php
@@ -4,7 +4,7 @@
 <section class="content">
     <div class="container">
         <div class="card">
-            <div class="card-header bg-info">{{ __('Integrantes Grupo 6') }}</div>
+            <div class="card-header bg-info">{{ __('Integrantes') }}</div>
 
             <div class="card-body">
                 <ul>
