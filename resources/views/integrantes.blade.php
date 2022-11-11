64fae20cef59223f3f58d47d5a32c865ecc266b3
jvalladolid365 <jvalladolid365@gmail.com>
2022-11-11 03:20:05 -0500

    Cambio de informacion de vista nosotros

--- resources/views/integrantes.blade.php
+++ resources/views/integrantes.blade.php
@@ -4,14 +4,15 @@
 <section class="content">
     <div class="container">
         <div class="card">
-            <div class="card-header bg-info">{{ __('Integrantes') }}</div>
+            <div class="card-header bg-info">{{ __('Integrantes Grupo 6') }}</div>
 
             <div class="card-body">
                 <ul>
-                    <li>Integrante 1</li>
-                    <li>Integrante 2</li>
-                    <li>Integrante 3</li>
-                    <li>Integrante 4</li>
+                    <li>Fierro Castro Hamms U19211659</li>
+                    <li>Angel Paucara Mamani U19313417</li>
+                    <li>Juan Jesus Lozano Vivar U20247690</li>
+                    <li>José Luis Valladolid Oregón U19213867</li>
+                    <li>Roberto Alonso Conislla Ramos U19300665</li>
                 </ul>
             </div>
         </div>
--- resources/views/nosotros.blade.php
+++ resources/views/nosotros.blade.php
@@ -8,7 +8,10 @@
 
             <div class="card-body">
                 <p>
-                    Mi Misión
+                Nuestra misión es crear sistemas de control eficaces para
+                nuestros clientes con el motivo de crecer como estudiantes
+                en nuestra carrera universitaria y también adquirir mas
+                experiencia en la construcción de sistemas y desarrollo web.
                 </p>
             </div>
         </div>
@@ -18,7 +21,10 @@
 
             <div class="card-body">
                 <p>
-                    Mi Visión
+                Nuestra visión con nuestro proyecto es que se mantenga un
+                orden de los productos que se ofrezcan dentro de la empresa
+                technoshark todo con la finalidad de que se mantenga un control
+                de calidad y ventas de manera eficiente.
                 </p>
             </div>
         </div>
@@ -28,7 +34,9 @@
 
             <div class="card-body">
                 <p>
-                    Mi Información
+                    Nuestra tienda de aparatos electrónicos Techno Shark tiene
+                    el propósito de vender productos de marcas reconocidas para
+                    una mejor calidad y asi tener la confianza de nuestros clientes.
                 </p>
             </div>
         </div>
